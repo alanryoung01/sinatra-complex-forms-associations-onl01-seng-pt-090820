de6ce377b34414e124b4ef64df82f2f5f9d4ea19
Alan Young <alanryoung01@gmail.com>
2020-12-10 15:48:54 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -9,9 +9,10 @@ class OwnersController < ApplicationController
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
-  end
+  post '/owners' do
+  @owner = Owner.create(params[:owner])
+  redirect "/owners/#{@owner.id}"
+end
 
   get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
